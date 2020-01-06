ef122bed6f1a996a88a47ce53ce775112520a919
reireias <reireias@gmail.com>
2020-01-06 22:59:41 +0900

    fix(functions): fix lint warning

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,8 @@
 const functions = require('firebase-functions')
 
-exports.postMesage = functions.region('asia-northeast1').https.onRequest((request, response) => {
-  console.log(request)
-  response.send('ok')
-})
+exports.postMesage = functions
+  .region('asia-northeast1')
+  .https.onRequest((request, response) => {
+    console.log(request)
+    response.send('ok')
+  })
