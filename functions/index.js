3bde1e77f5e111b62f7caed3dad8f7930816a7e4
reireias <reireias@gmail.com>
2020-01-06 22:18:16 +0900

    add https functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.postMesage = functions.region('asia-northeast1').https.onRequest((request, response) => {
+  console.log(request)
+  response.send('ok')
+})
