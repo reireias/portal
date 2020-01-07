89e73ca6daa59ea9d47d08b06c4c50b8a5c65611
reireias <reireias@gmail.com>
2020-01-07 22:45:35 +0900

    ci(functions): add functions test code

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions')
 
+const TOKEN_KEY = 'x-api-token'
+
 exports.postMessage = functions
   .region('asia-northeast1')
   .https.onRequest((request, response) => {
+    if (request.method !== 'POST') {
+      response.status(405).send('Method Not Allowed')
+      return
+    }
+    if (request.get(TOKEN_KEY) !== functions.config().message.token) {
+      response.status(403).send('Access Denied')
+      return
+    }
     console.log(request)
     response.send('ok')
   })
