4a9e01702ba5116eaa8680367baa3354f2f64785
reireias <reireias@gmail.com>
2020-05-19 22:55:18 +0900

    pdate lint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,10 @@ module.exports = {
   plugins: [
     'prettier'
   ],
-  ignorePatterns: ['client/'],
+  ignorePatterns: [
+    'client/',
+    'functions/',
+  ],
   // add your custom rules here
   rules: {
   }
