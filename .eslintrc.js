d302c794f478d230375e02f7c53e56636d855fc4
reireias <reireias@gmail.com>
2020-06-19 16:44:10 +0900

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
-  plugins: [
-    'prettier'
-  ],
-  ignorePatterns: [
-    'client/',
-    'functions/',
-  ],
+  plugins: ['prettier'],
+  ignorePatterns: ['client/', 'functions/'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {},
 }
